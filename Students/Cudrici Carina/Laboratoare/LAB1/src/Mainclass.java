f641c72a13abbeeff2d09f6ab1e1ecebab5a3b4c
carinaa28 <50113293+carinaa28@users.noreply.github.com>
2019-10-18 19:14:21 +0300

    lab1

---
+++ Students/Cudrici Carina/Laboratoare/LAB1/src/Mainclass.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class Mainclass {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Hello World");	
+	
+	int n=10;
+	Random rand= new Random();
+	int i=rand.nextInt(n);
+	int i2=i*2;
+	System.out.println(i2);
+
+	}
+
+}
